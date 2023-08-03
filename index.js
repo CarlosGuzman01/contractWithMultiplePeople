1dd50f295d78a98aa4b31d12bc4db20478ef443b
CarlosGuzman01 <carlosguzmantejeda13@gmail.com>
2023-08-03 14:17:39 -0400

    done

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ import { Contract, ElectrumNetworkProvider, SignatureTemplate } from 'cashscript
 import { URL } from 'url';
 
 
-// Import Bob and Alice's keys from common.js
+// Import Bob, Charlie, Alice's keys from common.js
 import {
   alicePriv,
   alicePub,
+  aliceAddress,
   bobPriv,
   bobPub,
+  bobAddress,
+  charliePriv,
+  charliePub,
+  charlieAddress,
 } from './common.js';
 
 
@@ -29,19 +34,36 @@ console.log('contract address:', contract.address);
 console.log('contract balance:', await contract.getBalance());
 
 // Call the transfer function with bob's signature
-// Allows bob to claim the money that alice sent him
+//Allows bob to claim the money that alice sent him
 const transferTx = await contract.functions
   .transfer(new SignatureTemplate(bobPriv))
-  .to(contract.address, 10000n)
+  .to(contract.address, 1000n)
+  .to(charlieAddress, 1000n)
+  .to(bobAddress, 1000n)
+  .to(aliceAddress, 1000n)
   .send();
 
 console.log('transfer transaction details:', stringify(transferTx));
 
+//CMGT1---------------------------------------------------------------------
+
+// const transferTX2 = await contract.functions
+// .transfer(new SignatureTemplate(charliePriv))
+// .to(contract.address, 1000n)
+// .send();
+
+
+// console.log("transfer transaction, charlie , details:", stringify(transferTX2));
+
+//CMGT2---------------------------------------------------------------------
+
+
+
 // Call the timeout function with alice's signature
 // Allows alice to reclaim the money she sent as the timeout is in the past
-// const timeoutTx = await contract.functions
-//   .timeout(new SignatureTemplate(alicePriv))
-//   .to(contract.address, 10000n)
-//   .send();
+const timeoutTx = await contract.functions
+  .timeout(new SignatureTemplate(alicePriv))
+  .to(contract.address, 10000n)
+  .send();
 
-// console.log('timeout transaction details:', stringify(timeoutTx));
+console.log('timeout transaction details:', stringify(timeoutTx));
